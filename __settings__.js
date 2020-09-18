0831ef177ad0e3f74fcff1efdd884ab44b47579c
yuukisann1 <yuki-iphone7abcd@ezweb.ne.jp>
2020-09-18 22:47:58 +0900

    first

---
+++ __settings__.js
@@ -0,0 +1,20 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "991714.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 35535881, 35535891, 35535887, 35535867, 35535868, 35535886, 35535871, 35535893, 35535875, 35535870, 35535883 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
